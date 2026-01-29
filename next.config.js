f283c8ba7e6594b0990c47828b7d69c5d2ee74c2
zetroxscorpion-ai <zetroxscorpion@gmail.com>
2026-01-29 22:16:06 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
